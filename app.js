31e4241dd15d84cc131ca5bcefc088187937a5d3
johnnyfwk <johnnyfong@live.com>
2023-02-20 10:47:25 +0000

    app.js: added path

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const cors = require('cors');
 
 app.use(express.json());
-
-const cors = require('cors');
 
 const {
     getUsers,
